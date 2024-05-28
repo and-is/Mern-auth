71f6f8ce6f3d2913d99c6e2febb6237ec6df0b20
and-is <andispaudel@gmail.com>
2024-05-28 15:07:30 +0545

    built server side

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
